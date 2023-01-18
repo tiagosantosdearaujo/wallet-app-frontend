13255bd4856e290686622bf4d4ffce343944cd04
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-01-17 18:02:23 -0800

    implement header at home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,31 @@
-window.onload = () => {
+const onLoaduserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
-  console.log(email);
+  const name = localStorage.getItem("@WalletApp:userName");
+
+  const navbarUserInfo = document.getElementById("navbar-user-container");
+  const navbaruserAvatar = document.getElementById("navbar-user-avatar");
+
+  // add user email
+  const emailElement = document.createElement("p");
+  const emailText = document.createTextNode(email);
+  emailElement.appendChild(emailText);
+  navbarUserInfo.appendChild(emailElement);
+
+  // add logout link
+  const logoutElement = document.createElement("a");
+  const logoutText = document.createTextNode("sair");
+
+  logoutElement.appendChild(logoutText);
+  navbarUserInfo.appendChild(logoutElement);
+
+  // add user first letter inside avatar
+  const nameElement = document.createElement("h3");
+  const nameText = document.createTextNode(name.charAt(0));
+
+  nameElement.appendChild(nameText);
+  navbaruserAvatar.appendChild(nameElement);
+};
+
+window.onload = () => {
+  onLoaduserInfo();
 };
